ce236194732e2e5dc78d6223b2bf65accc55f6b5
cactusballs <lottielucyjudge@gmail.com>
2024-11-19 20:55:17 +0000

    implementing reset button (no functionality yet)

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ function handleUpdate(){
 
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
 inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
+const reverts = document.getElementById('.restore');
+
+const reset = () => {
+  document.documentElement.style.setProperty = ' ';
+}
+
+reverts.forEach(revert => revert.addEventListener('onclick', reset))
